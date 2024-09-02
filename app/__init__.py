3deefd93cebffc65b50a22d8fe47b48e16af5be1
imonbanerjeetf <imon.banerjee@thirdfeed.com>
2024-09-02 20:39:08 +0530

    pdf stuff and requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+import os
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
 
 from app import routes
